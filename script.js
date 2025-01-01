63052877645aec0cbabaabbbca6c6861481804c3
Kea-Tan <hilariavalencia284@gmail.com>
2025-01-02 03:13:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+// Optional: Add interactivity or dynamic gallery features
+document.querySelectorAll('.painting').forEach((painting, index) => {
+    painting.addEventListener('click', () => {
+        alert(You clicked on Artwork ${index + 1});
+    });
+});
